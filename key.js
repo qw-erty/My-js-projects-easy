f112049e3932db7cb73db91095c626b385a1e659
abhi <forgames.farzi@gmail.com>
2021-04-20 14:10:21 +0530

    javascript file

---
+++ key.js
@@ -0,0 +1,292 @@
+var btn1 = document.getElementById('b1');
+var btn2 = document.getElementById('b2');
+var btn3 = document.getElementById('b3');
+var btn4 = document.getElementById('b4');
+var btn5 = document.getElementById('b5');
+var btn6 = document.getElementById('b6');
+var btn7 = document.getElementById('b7');
+var btn8 = document.getElementById('b8');
+var btn9 = document.getElementById('b9');
+
+
+var a1 = document.getElementById('a1');
+var a2 = document.getElementById('a2');
+var a3 = document.getElementById('a3');
+var a4 = document.getElementById('a4');
+var a5 = document.getElementById('a5');
+var a6 = document.getElementById('a6');
+var a7 = document.getElementById('a7');
+var a8 = document.getElementById('a8');
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 49)
+    {
+        var h = parseInt(window.getComputedStyle(btn1).height);
+        btn1.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 49)
+    {
+        var h = parseInt(window.getComputedStyle(btn1).height);
+        btn1.style.height= "200px";
+        btn1.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key === 49)
+    {
+        var h = parseInt(window.getComputedStyle(btn1).height);
+        btn1.style.height= "205px";
+        btn1.style.boxShadow= "5px 5px yellow";
+        a1.play();
+    }
+})
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 50)
+    {
+        var h = parseInt(window.getComputedStyle(btn2).height);
+        btn2.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 50)
+    {
+        var h = parseInt(window.getComputedStyle(btn2).height);
+        btn2.style.height= "200px";
+        btn2.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key === 50)
+    {
+        var h = parseInt(window.getComputedStyle(btn2).height);
+        btn2.style.height= "205px";
+        btn2.style.boxShadow= "5px 5px yellow";
+        a2.play();
+    }
+})
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 51)
+    {
+        var h = parseInt(window.getComputedStyle(btn3).height);
+        btn3.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 51)
+    {
+        var h = parseInt(window.getComputedStyle(btn3).height);
+        btn3.style.height= "200px";
+        btn3.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key === 51)
+    {
+        var h = parseInt(window.getComputedStyle(btn3).height);
+        btn3.style.height= "205px";
+        btn3.style.boxShadow= "5px 5px yellow";
+        a3.play();
+    }
+})
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 52)
+    {
+        var h = parseInt(window.getComputedStyle(btn4).height);
+        btn4.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 52)
+    {
+        var h = parseInt(window.getComputedStyle(btn4).height);
+        btn4.style.height= "200px";
+        btn4.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key === 52)
+    {
+        var h = parseInt(window.getComputedStyle(btn4).height);
+        btn4.style.height= "205px";
+        btn4.style.boxShadow= "5px 5px yellow";
+        a4.play();
+    }
+})
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 53)
+    {
+        var h = parseInt(window.getComputedStyle(btn5).height);
+        btn5.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 53)
+    {
+        var h = parseInt(window.getComputedStyle(btn5).height);
+        btn5.style.height= "200px";
+        btn5.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key === 53)
+    {
+        var h = parseInt(window.getComputedStyle(btn5).height);
+        btn5.style.height= "205px";
+        btn5.style.boxShadow= "5px 5px yellow";
+        a5.play();
+    }
+})
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 54)
+    {
+        var h = parseInt(window.getComputedStyle(btn6).height);
+        btn6.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 54)
+    {
+        var h = parseInt(window.getComputedStyle(btn6).height);
+        btn6.style.height= "200px";
+        btn6.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key === 54)
+    {
+        var h = parseInt(window.getComputedStyle(btn6).height);
+        btn6.style.height= "205px";
+        btn6.style.boxShadow= "5px 5px yellow";
+        a6.play();
+    }
+})
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 55)
+    {
+        var h = parseInt(window.getComputedStyle(btn7).height);
+        btn7.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 55)
+    {
+        var h = parseInt(window.getComputedStyle(btn7).height);
+        btn7.style.height= "200px";
+        btn7.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key === 55)
+    {
+        var h = parseInt(window.getComputedStyle(btn7).height);
+        btn7.style.height= "205px";
+        btn7.style.boxShadow= "5px 5px yellow";
+        a7.play();
+    }
+})
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 56)
+    {
+        var h = parseInt(window.getComputedStyle(btn8).height);
+        btn8.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 56)
+    {
+        var h = parseInt(window.getComputedStyle(btn8).height);
+        btn8.style.height= "200px";
+        btn8.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key ===56)
+    {
+        var h = parseInt(window.getComputedStyle(btn8).height);
+        btn8.style.height= "205px";
+        btn8.style.boxShadow= "5px 5px yellow";
+        a8.play();
+    }
+})
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+document.body.addEventListener('keydown', function(e){
+    var key= e.keyCode;
+    if(key === 57)
+    {
+        var h = parseInt(window.getComputedStyle(btn9).height);
+        btn9.style.height= "208px";
+    }
+})
+
+document.body.addEventListener('keyup', function(e){
+    var key= e.keyCode;
+    if(key === 57)
+    {
+        var h = parseInt(window.getComputedStyle(btn9).height);
+        btn9.style.height= "200px";
+        btn9.style.boxShadow= "5px 5px rgba(0,0,0,0.2)";
+        
+    }
+})
+document.body.addEventListener('keypress', function(e){
+    var key= e.keyCode;
+    if(key === 57)
+    {
+        var h = parseInt(window.getComputedStyle(btn9).height);
+        btn9.style.height= "205px";
+        btn9.style.boxShadow= "5px 5px yellow";
+    }
+})
